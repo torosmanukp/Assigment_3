365141f83c94960661d05f757b40cd1e8bd39498
Toros-Manuk <toros.manuk21@gmail.com>
2026-03-24 23:23:52 +0200

    added the data from the task

--- task2.py
+++ task2.py
@@ -9,4 +9,15 @@ def get_successful_students(students_list, passing_score=60):
             succes_students[name] = sum(grades) / len(grades)
     return succes_students
 
+students_math_results = [
+{"name": "Олександр", "scores": {"Calculus": 85,
+"Algebra": 90, "Discrete Math": 78}},
+{"name": "Марія", "scores": {"Calculus": 65, "Algebra":
+55, "Discrete Math": 70}},
+{"name": "Іван", "scores": {"Calculus": 92, "Algebra": 88,
+"Discrete Math": 95}},
+{"name": "Анна", "scores": {"Calculus": 45, "Algebra": 60,
+"Discrete Math": 50}}
+]
+
 
