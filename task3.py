d237f06d1e8b6c7b09e147cf5d299fe425c308d1
Toros-Manuk <toros.manuk21@gmail.com>
2026-03-24 23:54:49 +0200

    added the input data

---
+++ task3.py
@@ -0,0 +1,17 @@
+
+
+experiments_data = [
+{"method": "Euler", "iteration": 1, "error": 0.15,
+"time_ms": 1.2},
+{"method": "Runge-Kutta", "iteration": 1, "error": 0.01,
+"time_ms": 3.5},
+{"method": "Euler", "iteration": 2, "error": 0.18,
+"time_ms": 1.3},
+{"method": "Runge-Kutta", "iteration": 2, "error": 0.008,
+"time_ms": 3.6},
+{"method": "Euler", "iteration": 3, "error": 0.22,
+"time_ms": 1.2},
+{"method": "Newton", "iteration": 1, "error": 0.05,
+"time_ms": 2.1}
+]
+
