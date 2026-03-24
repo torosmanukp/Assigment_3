1d5c9049936be7da61ebc213b1aa9a1bace74617
Toros-Manuk <toros.manuk21@gmail.com>
2026-03-24 22:57:29 +0200

    completed the task and checked for bugs everything works :)

--- task1.py
+++ task1.py
@@ -8,3 +8,6 @@ my_poly = {0: -10, 2: 3, 4: 1}
 
 x1=2
 x2=-1.5
+
+print(f'For x = {x1}, the result is {evaluate_polynomial(my_poly, x1)}')
+print(f'For x = {x2}, the result is {evaluate_polynomial(my_poly, x2)}')
